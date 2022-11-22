6da895ad383e312cd158b3c5ca9691a6d2f72aa1
Thomas Pelowitz <tjp00004@gmail.com>
2022-11-22 11:48:54 -0600

    Update streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+
+def supertype(x):
+    output = x.split('—') #split at dash
+    output = output[0].replace('Legendary', '').replace('World', '') #remove unimportant / outdated catergories in supertype
+    return output.strip() #final clean
+
+getYear = lambda x: int(x.split('-')[0])
+
+def colorFull(x):
+    if x == "['W']":
+        return 'White'
+    elif x == "['U']":
+        return 'Blue'
+    elif x == "['B']":
+        return 'Black'
+    elif x == "['R']":
+        return 'Red'
+    elif x == "['G']":
+        return 'Green'
+    elif x == "['CL']":
+        return 'Colorless'
+    elif x == "['MC']":
+        return 'Multi-colored'
+    else:
+        return x
+
+df = pd.read_csv('./top100salt.csv')
+
+df['supertype'] = df['type'].apply(supertype)
+df['year'] = df['date'].apply(getYear)
+
+st.title('Magic the Gathering "Salt" Analysis')
+st.write("Magic the Gathering is a popular trading card game produced by Wizards of the Coast. The game is renowned for it's complexitiy, and the ability for player to \
+    play the way that they want. When a card is commonly used in a way that inspires annoyance in other players, that card is labeled as a 'salty' card. Every year, a  \
+    popular Magic the Gathering site polls players on the top 100 cards that they think are the saltiest. The average score and count of votes for these cards give \
+    these cards a salt score. Below is an anyalsis on common attributes between these cards.")
