3ec1cd3193e2e81560a44bef0fa3c20841eae8de
Thomas Pelowitz <tjp00004@gmail.com>
2022-11-22 13:02:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 def supertype(x):
     output = x.split('—') #split at dash
@@ -32,7 +33,11 @@ df['supertype'] = df['type'].apply(supertype)
 df['year'] = df['date'].apply(getYear)
 
 st.title('Magic the Gathering "Salt" Analysis')
-st.write("Magic the Gathering is a popular trading card game produced by Wizards of the Coast. The game is renowned for it's complexitiy, and the ability for player to \
+st.write("Magic the Gathering is a popular trading card game produced by Wizards of the Coast. The game is renowned for its complextiy, and the ability for player to \
     play the way that they want. When a card is commonly used in a way that inspires annoyance in other players, that card is labeled as a 'salty' card. Every year, a  \
     popular Magic the Gathering site polls players on the top 100 cards that they think are the saltiest. The average score and count of votes for these cards give \
-    these cards a salt score. Below is an anyalsis on common attributes between these cards.")
+    these cards a salt score. Below is an analysis on common attributes between these cards.")
+
+corrTable = px.imshow(df.corr(), text_auto=True, height=800, width=800, template='ggplot2', aspect='auto', title='Correlation of Numerical Columns')
+corrTable.update_layout(title_x=0.5)
+st.plotly_chart(corrTable, use_container_width = True)
