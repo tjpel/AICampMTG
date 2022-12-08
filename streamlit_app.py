38670833317a9045261ffd519682f2e2ced2d472
Thomas Pelowitz <tjp00004@gmail.com>
2022-12-08 12:21:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,18 +35,24 @@ df['year'] = df['date'].apply(getYear)
 st.title('Magic the Gathering "Salt" Analysis')
 st.write("Magic the Gathering is a popular trading card game produced by Wizards of the Coast. The game is renowned for its complextiy, and the ability for player to \
     play the way that they want. When a card is commonly used in a way that inspires annoyance in other players, that card is labeled as a 'salty' card. Every year, a  \
-    popular Magic the Gathering site polls players on the top 100 cards that they think are the saltiest. The average score and count of votes for these cards give \
-    these cards a salt score. Below is an analysis on common attributes between these cards.")
+    popular Magic the Gathering site called EDHRec.com polls players on the top 100 cards that they think are the saltiest. The average score and count of votes for these \
+    cards give these cards a salt score. Below is an analysis on common attributes between these cards.")
+
+st.write("In the table below, 'score' refers to the score that the card was given on EDHRec. CMC is the converted mana cost, or how many resources the card costs to play. \
+    Price is what the card costs to buy in USD, and the year is the year the card was first printed.")
 
 corrTable = px.imshow(df.corr(numeric_only=True), text_auto=True, height=800, width=800, template='ggplot2', aspect='auto', title='Correlation Table')
 corrTable.update_layout(title_x=0.5)
 st.plotly_chart(corrTable, use_container_width = True)
 
-st.write("As we can see from this table, the only variable th")
+st.write("As we can see from this table, there is only a significant correlation in between the CMC and the price, suggesting that cards with a lower CMC are often \
+     more expensive. We can see that, to a lesser extent, cards with older first printing dates are more expensive to buy.")
 
 stvc = df['supertype'].value_counts()
-barFig = px.bar(stvc, x=stvc.index, y=stvc.values)
+barFig = px.bar(stvc, x=stvc.index, y=stvc.values, title="Count of Supertypes in the Top 100 Saltiest Cards", labels={'index': 'Supertype', 'y': 'Count'})
 st.plotly_chart(barFig)
+
+st.write("This chart shows that ")
 
 df['coloridfull'] = df['colorid'].apply(colorFull)
 pieFig = px.pie(df, values='score', names='coloridfull', color='coloridfull',
